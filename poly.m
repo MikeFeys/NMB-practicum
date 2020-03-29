d6fcebcd105c1589566ef4906eef8fa734de250d
Mike <mikefeys@hotmail.be>
2020-03-29 17:11:15 +0200

    Small visual update

--- poly.m
+++ poly.m
@@ -4,5 +4,5 @@ MatrixWaarden=zeros(size(a,2));%Preallocate for speed.
 for i =1:size(a,2)
    MatrixWaarden(i,:)=a(i)*T(i,:);%Scale the chebychev values in matrix T with vector a: element to row wise.
 end
-c=sum(MatrixWaarden,1);%Sum all columns to get the correct yn values.
+c=sum(MatrixWaarden,1);%Sum all columns to get the correct yn values. Coef in ascending order of X
 end
