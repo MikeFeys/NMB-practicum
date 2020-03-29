c3754b2c64498c29a09c2439f4b7ea25c1e0cae4
michiel <michieldebaets@hotmail.be>
2020-03-29 17:19:52 +0200

    Poly
    Inputproblemen oplossen

--- poly.m
+++ poly.m
@@ -1,5 +1,12 @@
 function [c] = poly(a, T)
+% controle valid input
+if size(T,1) - length(a) <0
+    disp('ERROR: niet genoeg cheb veeltermen voor de a-vector')
+    return;
+end
 %a is vect with k values
+a = [a, zeros(1, size(T,1) - length(a))];
+
 MatrixWaarden=zeros(size(a,2));%Preallocate for speed.
 for i =1:size(a,2)
    MatrixWaarden(i,:)=a(i)*T(i,:);%Scale the chebychev values in matrix T with vector a: element to row wise.
