9f9b7261ac5802f4602a913a3b21cd2d8a9e65e7
michiel <michieldebaets@hotmail.be>
2020-04-02 13:48:59 +0200

    Auto stash before merge of "master" and "origin/master"

--- main_.m
+++ main_.m
@@ -1,3 +1,43 @@
+%% 1.2.3 f(x) benaderen + convergentiesnelheid enzo
+syms x;
+f1(x) = (x-1)./(1+6.*x.^2);
+
+f_handle = @(x) (x-1)./(1+6.*x.^2);
+n_max =100;
+T = cheb(n_max);
+
+aantal_ev = 100;
+X = linspace(-1,1,aantal_ev);
+for n=2:n_max
+    a = chebcoeff(f_handle,n);
+    c = poly(a,T);
+    %polyval(fliplr(c),X)
+    
+    %plot(X, abs((X-1)./(1+6.*X.^2)-polyval(fliplr(c),X)));
+end
+    plot(X, abs((X-1)./(1+6.*X.^2)-polyval(fliplr(c),X)));
+
+
+
+%%
+% plotten
+aantal_ev = 100;
+X = linspace(-1,1,aantal_ev);
+y = zeros(1,aantal_ev);
+for i=1:n+1
+    y = y + c(i)*(X.^(i-1));
+end
+    %ylabel('Amplitude');
+    %xlabel('Frequentie (Hz)')
+figure()
+plot(X,y)
+if n==1
+    title(['Chebychev veeltermbenadering van: ', strrep(char(f_handle),'@(x)','') ,' tot op 1ste orde.'])
+else
+    title(['Chebychev veeltermbenadering van: ', strrep(char(f_handle),'@(x)','') ,' tot op ',num2str(n),'de orde.'])
+end
+
+
 %% plotting cheb
 n=5;
 tic
