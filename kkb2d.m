645ef72c882f7c17ee3982299533df8d6592a990
Mike <mikefeys@hotmail.be>
2020-04-12 11:35:35 +0200

    2.2.4 partly done

--- kkb2d.m
+++ kkb2d.m
@@ -7,14 +7,12 @@ N=length(y);
 A=zeros(M,m+1);%Preallocate for speed
 B=zeros(N,n+1);%Preallocate for speed
 if m == n
-    if m == n
         for i=1:M %For loop adds row per row to matrix A with xi values calculated
             for j=0:m
                 A(i,j+1)=x(i)^j;
                 B(i,j+1)=y(i)^j;
             end
         end
-    end
 else
     for i=1:M %For loop adds row per row to matrix A with xi values calculated
         for j=0:m
