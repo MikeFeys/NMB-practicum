4383a770757b8185f04874330df2eededb813256
Mike <mikefeys@hotmail.be>
2020-04-12 15:43:45 +0200

    kkb2d en poly commentaar

--- kkb2d.m
+++ kkb2d.m
@@ -1,30 +1,44 @@
 function [C] = kkb2d(x, y, F, m, n) 
-%Berekend de coefficientenmatrix c
-%x = [x1...xM] en y = [y1...yN], cartesisch product geeft puntenrooster
-% F matrix en m,n veeltermgraden
+%Geeft de coefficientenmatrix C terug
+% Input:
+%   x = [x1...xM] en y = [y1...yN] waarvan cartesisch product puntenrooster geeft
+%   F matrix en m,n veeltermgraden
+
+%Waarden toekennen.
 M=length(x);
 N=length(y);
-A=zeros(M,m+1);%Preallocate for speed
-B=zeros(N,n+1);%Preallocate for speed
+
+%Preallocate voor snelheid
+A=zeros(M,m+1);
+B=zeros(N,n+1);
+
+%Het if statement zorgt voor een snellere berekening indien bepaalde input waarden gelijk
+%zijn.
 if m == n && M==N
-        for i=1:M %For loop adds row per row to matrix A with xi values calculated
+        %Indexeren over de rijen.
+        for i=1:M 
+            %Indexeren over de elementen in de rij en bijhorende waarden toekennen vanuit de monomiale basis.
             for j=0:m
                 A(i,j+1)=x(i)^j;
                 B(i,j+1)=y(i)^j;
             end
         end
 else
-    for i=1:M %For loop adds row per row to matrix A with xi values calculated
+     %Indexeren over de rijen.
+    for i=1:M 
+        %Indexeren over de elementen in de rij en bijhorende waarden toekennen vanuit de monomiale basis.
         for j=0:m
             A(i,j+1)=x(i)^j;
         end
     end
-    for i=1:N %For loop adds row per row to matrix B with yi values calculated
+     %Indexeren over de rijen.
+    for i=1:N 
+        %Indexeren over de elementen in de rij en bijhorende waarden toekennen vanuit de monomiale basis.
         for j=0:n
             B(i,j+1)=y(i)^j;
         end
     end
 end
-C=pinv(A)*F*transpose(pinv(B)); %pinv() gives pseudoinverse of matrix
-%Still have to do some shitty tests
+%Coefficientenmatrix berekenen.
+C=pinv(A)*F*transpose(pinv(B));
 end
